f4b992d9e1569c28c4ecfad3b13856fe54e9fca0
pointbar <stephane.langlois@scopyleft.fr>
2016-06-18 17:12:55 +0200

    Add anchors behaviours

--- src/tpl-contribution.js
+++ src/tpl-contribution.js
@@ -1,10 +1,11 @@
 {
   const html = ({link, label, content, prose_url, git_url}) => `
+    <a name="top"></a>
     <aside class="contribution-tools">
       <a href="${git_url}" class="github-link">Voir sur Github</a>
       <a href="${prose_url}" class="proseio-link">Editer sur prose.io</a>
-      <a href="" class="help-link">Aide</a>
-      <a href="" class="page-top">Haut de page</a>
+      <a href="#multibao/documentation/blob/master/README.md" class="help-link">Aide</a>
+      <a href="#top" class="page-top">Haut de page</a>
     </aside>
     <div id="parentRepo" class="breadcrumbs">
       À retrouver dans le dépôt : <a href="${link}">${label}</a>
@@ -13,7 +14,7 @@
       ${content}
     </article>
   `
-  template.create('contribution')
+  template.contribution = new Template('contribution')
   template.contribution.data = () => {
     const ghApi = new GithubUrl(router.params)
     ghApi.getHtmlBlob().then(htmlResponse => {
