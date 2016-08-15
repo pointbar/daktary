68f299ce940edde6ef12aedc7ae1c4fbb1baff9c
portail de données ouvertes datalocale.fr <datalocale@cg33.fr>
2016-08-15 15:41:56 +0200

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ GH.OWNER = 'multibao'
 GH.CREW = 'organisations'
 
 var MULTIBAO = {}
-MULTIBAO.UVP1 = 'S\'inspirer des autres cultures'
+MULTIBAO.UVP1 = 'Apprendre avec de la junk data'
 MULTIBAO.UVP2 = 'Rédiger et partager vos expériences'
 MULTIBAO.BUTTON1 = 'Commencer ici'
 MULTIBAO.BUTTON2 = 'Guide d\'utilisation'
